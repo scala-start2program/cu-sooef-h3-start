0674c7ef7dabf197f59ff24a52f34cdfc4cb02c3
jandedeurwaerder-howest <jan.de.deurwaerder@howest.be>
2023-04-27 13:34:19 +0200

    Add project files.

---
+++ Scala.ClassLibrary.Core/Persoon.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Scala.ServiceClasses.Core
+{
+    public class Persoon
+    {
+        public string Naam { get; set; }
+        public string Voornaam { get; set; }
+        public DateTime Geboortedatum { get; set; }
+        public bool IsMan { get; set; }
+
+        public Persoon()
+        {
+        }
+        public Persoon(string naam, string voornaam, DateTime geboortedatum, bool isman)
+        {
+            Naam = naam;
+            Voornaam = voornaam;
+            Geboortedatum = geboortedatum;
+            IsMan = isman;
+        }
+
+        public override string ToString()
+        {
+            return $"{Naam} {Voornaam}";
+        }
+        public string GetLeeftijd()
+        {
+            DateTime vandaag = DateTime.Now;
+            int jaren = vandaag.Year - Geboortedatum.Year;
+            int maanden = vandaag.Month - Geboortedatum.Month;
+            int dagen = vandaag.Day - Geboortedatum.Day;
+
+            if (maanden < 0 || (maanden == 0 && dagen < 0))
+            {
+                jaren--;
+                maanden += 12;
+                if (dagen < 0)
+                {
+                    dagen += DateTime.DaysInMonth(Geboortedatum.Year, Geboortedatum.Month);
+                }
+            }
+            return $"{jaren} jaar, {maanden} maanden en {dagen} dagen";
+        }
+
+    }
+}
---
+++ Scala.ServiceClasses.Wpf/MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Scala.ServiceClasses.Core;
+
+namespace Scala.ServiceClasses.Wpf
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void LstPersonen_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+        }
+
+        private void BtnNieuw_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void BtnWijzig_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+
+        private void BtnWis_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void BtnBewaren_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void BtnAnnuleren_Click(object sender, RoutedEventArgs e)
+        {
+        }
+    }
+}
